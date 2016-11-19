7ccd5666696ac3a3da84d29d117c922c1a75dea1
Kevin Mach <mr.kevinmach@gmail.com>
2016-11-18 16:05:39 -0800

    Aded Comic Book and Artist Models

---
+++ src/ComicBookGallery/Models/Artists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artists
+    {
+        public String Name { get; set; }
+        public String Role { get; set; }
+    }
+}
